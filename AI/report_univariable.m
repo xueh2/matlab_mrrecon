f3fc6354dfb981022b88fb9bcc0fc2278b95bd77
Hui Xue <hui.xue@nih.gov>
2023-10-07 15:08:02 -0400

    update AI/report_univariable.m

--- AI/report_univariable.m
+++ AI/report_univariable.m
@@ -1,4 +1,4 @@
-function [min_v, max_v, median_v, v_0p25, v_0p75] = report_univariable(v, desp)
+function [min_v, max_v, median_v, v_0p05, v_0p95] = report_univariable(v, desp)
 
 ind = find(v>0);
 
@@ -8,7 +8,7 @@ min_v = min(v(ind));
 max_v = max(v(ind));
 median_v = median(v(ind));
 r = quantile(v(ind),[0.05 0.95]);
-v_0p25 = r(1);
-v_0p75 = r(2);
+v_0p05 = r(1);
+v_0p95 = r(2);
 
 disp([desp ' - mean +/- std = ' num2str(mean_v) '+/-' num2str(std_v) ' - min = ' num2str(min(v(ind))) ' - max = ' num2str(max(v(ind))) ' - median = ' num2str(median(v(ind))) ' - 5% - 95% = ' num2str(prctile(v(ind), 5)) ' - ' num2str(prctile(v(ind), 95))]);
